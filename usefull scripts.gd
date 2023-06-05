7df9bec12e816cb163a183fa18d1317443e72290
massagames <massagames@gmail.com>
2023-06-05 12:10:30 +0300

    Update and rename usefull scripts to usefull scripts.gd

---
+++ usefull scripts.gd
@@ -0,0 +1,11 @@
+$Control/NewRecord/text_best_time.text = String(stepify(time_best, 0.01)) + " sec" - вывод числа с троку с точностью 0.01
+
+onready var text_total_stars = $Control/stars_total - 
+
+Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) - делать курсор мыши видимым
+
+var starSrc = preload("res://sfxs/sfx_plane_crash.tscn") - прелоад необходим для всех партикл эффектов
+var star = starSrc.instance()
+		add_child(star)
+		star.position = Vector2 (rand_range(200, 1050), rand_range(300, 2350))
+    
