45077fc87fbaa0a515dadd03f90c7e919a83522a
massagames <massagames@gmail.com>
2023-06-14 19:25:06 +0300

    usefull scripts.gd

--- usefull scripts.gd
+++ usefull scripts.gd
@@ -1,14 +1,15 @@
-$Control/NewRecord/text_best_time.text = String(stepify(time_best, 0.01)) + " sec" - вывод числа с троку с точностью 0.01
+$Control/NewRecord/text_best_time.text = String(stepify(time_best, 0.01)) + " sec" # вывод числа с троку с точностью 0.01
 
-onready var text_total_stars = $Control/stars_total - присваивание при загрузке скрипта
+onready var text_total_stars = $Control/stars_total # присваивание при загрузке скрипта
+export (NodePath) onready var gbox = get_node(gbox) as CSGBox # ссылка на внешний объект определенного типа
 
-enum State {IDLE, RUN, AWAY} - перечисление
-var state = State.IDLE - задаем значение переменной
-change_state(State.RUN) - можно подавать в функцию
+enum State {IDLE, RUN, AWAY} # перечисление
+var state = State.IDLE # задаем значение переменной
+change_state(State.RUN) # можно подавать в функцию
 
-Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) - делать курсор мыши видимым
+Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) # делать курсор мыши видимым
 
-var starSrc = preload("res://sfxs/sfx_plane_crash.tscn") - прелоад необходим для всех партикл эффектов
+var starSrc = preload("res://sfxs/sfx_plane_crash.tscn") # прелоад необходим для всех партикл эффектов
 var star = starSrc.instance()
 	add_child(star)
 	star.position = Vector2 (rand_range(200, 1050), rand_range(300, 2350))
@@ -30,3 +31,4 @@ func run_animations():
 	await get_tree().create_timer(0.5).timeout
 	queue_free()
 	print("finish")
+
